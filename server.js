bc9db3638bb7bb8a0c6884117524c79a9aaeaaa1
Ergin <ergin_u@hotmail.com>
2016-07-10 19:58:37 +0000

    second commit.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var platform = require('platform');
+var useragent = require("useragent")
+var requestLanguage = require('express-request-language');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get("/", function(req, res){
+    var ip = req.headers['x-forwarded-for'].split(",")[0];
+    var agent = useragent.parse(req.headers['user-agent']);
+    var language = req.headers['accept-language']
+    
+    var result = {
+        "ipadress": ip,
+        "OS": agent.os.toString(),
+        "Version": agent.os.toVersion(),
+        "Language": language.split(",")[0]
+    }
+    res.end(JSON.stringify(result));
+    console.log(ip)
+    console.log(useragent.parse(req.headers['user-agent']).toString());
+    console.log(req.headers["accept-language"])
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
